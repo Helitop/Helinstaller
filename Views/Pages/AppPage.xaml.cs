efbadc73e4399ffadba2eeb80a3515e39fe772a5
Алексей Чащин <Helitopplaytm@gmail.com>
2025-10-30 18:54:34 +0700

    Обновлено

--- Views/Pages/AppPage.xaml.cs
+++ Views/Pages/AppPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using Wpf.Ui.Abstractions.Controls;
@@ -25,6 +26,11 @@ namespace Helinstaller.Views.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             ViewModel.CheckCommand.Execute(null);
+        }
+
+        private void InstallButton_Click(object sender, RoutedEventArgs e)
+        {
+            ForceInstallSwitch.IsChecked = false;
         }
     }
 }
